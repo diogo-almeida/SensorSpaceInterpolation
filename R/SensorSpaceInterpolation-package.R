e591c87b18be512ad35217077f29851535605d7e
Diogo Almeida <dalazal@gmail.com>
2015-06-02 22:27:49 +0400

    Added short package description.

---
+++ R/SensorSpaceInterpolation-package.R
@@ -0,0 +1,11 @@
+#' @title Set of functions to perform sensor space interpolation of EEG/MEG data.
+#' 
+#' @description This package implements linear, cubic and thin-plate spline
+#'   interpolation of sensor data from EEG/MEG recordings, allowing the plotting
+#'   of the estimated electric/magnetic fields recorded from brain activity.
+#'
+#' @name SensorSpaceInterpolation-package
+#' @docType package
+#' @author Diogo Almeida \email{diogo@@nyu.edu}
+#' @keywords package
+NULL
